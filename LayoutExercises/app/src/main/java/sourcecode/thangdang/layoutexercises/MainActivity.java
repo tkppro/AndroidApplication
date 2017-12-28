33d6c6fb89d74e7200374f70dd94d58430a78161
ThangDang <tkpproo@gmail.com>
2017-12-29 00:03:28 +0700

    Updated bai3 LayoutExercises

--- LayoutExercises/app/src/main/java/sourcecode/thangdang/layoutexercises/MainActivity.java
+++ LayoutExercises/app/src/main/java/sourcecode/thangdang/layoutexercises/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.bai3_layout_4);
+        setContentView(R.layout.bai3_layout_2);
     }
 }
